42bba52e4b0f13d93f14b315ab94003db4e940d4
irina.p.10.03@gmail.com <HInB35QiiNzsaHXISW3V>
2023-05-31 23:17:35 +0200

    31/05

--- src/main/java/by/mlechka/xml/main/DomMain.java
+++ src/main/java/by/mlechka/xml/main/DomMain.java
@@ -3,19 +3,9 @@ package by.mlechka.xml.main;
 import by.mlechka.xml.builder.SweetsDomBuilder;
 import by.mlechka.xml.reader.PropertiesStreamReader;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DomMain {
 
